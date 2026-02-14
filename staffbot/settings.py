04fce1c4dc8d4c795ac623a1f778b8da0df02739
Harashish Singh Rohewal <singhharashish@gmail.com>
2026-02-15 00:24:05 +0530

    Add requirements for Render deployment ficed

--- staffbot/settings.py
+++ staffbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2-#3$n4m$zylg(4vlj^2pul5qakl=c2=_ih&lyb2f4&ej)ero$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
